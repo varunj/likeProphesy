66dcbb1ec2aa3f42767f6ccbe81e0b4c137a6c21
Varun Jain <varun14170@iiitd.ac.in>
2015-12-01 23:14:41 +0530

    Added Progress Bar

--- src/likeprophesy/StartFXMLController.java
+++ src/likeprophesy/StartFXMLController.java
@@ -5,7 +5,10 @@
  */
 package likeprophesy;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -13,6 +16,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -28,7 +33,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import static likeprophesy.LikeProphesy.countlikesFrom1;
 import static likeprophesy.LikeProphesy.countlikesFrom2;
 import static likeprophesy.LikeProphesy.countlikesFrom3;
@@ -42,9 +51,10 @@ import static likeprophesy.LikeProphesy.result1;
 public class StartFXMLController implements Initializable {
     
     String accessToken = "CAACEdEose0cBAASspZC5PZA3IV9pD6hSgvnEHV92FeYaBctX4sXplPADDleCEQZBPEqIHndDPpcBg7ZC81W2kqJD0w7ARW1WgJjEXYZBVh0Sr2EaUDx6uVGz0Yf6sMG7CDl43NzDMf1BeQJX6XekW71lpWDNE0oxmKe0eVrlcZARSsaxZBKs8GUx3rcdl2f4GGPc1bebx49bQZDZD";
+    String imagePath = "";
     
     @FXML
-    private TextField t1;
+    private TextArea t1;
     @FXML
     private Button jButton1;
     @FXML
@@ -57,7 +67,12 @@ public class StartFXMLController implements Initializable {
     private BarChart<String, Double> graph;
     @FXML
     private Label sliderLabel;
-
+    @FXML
+    private ImageView iv1;
+    @FXML
+    private Button UploadButton;
+    @FXML
+    private ProgressBar progressBar;
     /**
      * Initializes the controller class.
      */
@@ -73,7 +88,7 @@ public class StartFXMLController implements Initializable {
         slider.setMajorTickUnit(50);
         slider.setMinorTickCount(5);
         slider.setBlockIncrement(10);
-        
+              
         slider.valueProperty().addListener(new ChangeListener<Number>() 
         {
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) 
@@ -87,10 +102,15 @@ public class StartFXMLController implements Initializable {
     }    
 
     @FXML
-    private void goButtonClick(ActionEvent event) {
-
+    private void goButtonClick(ActionEvent event)throws InterruptedException  {
+        for (int i = 0; i < 6; i++)
+        {
+            progressBar.setProgress((i*1.0)/10);
+            Thread.sleep(90);
+        }
         // predict
         LikeProphesy.prophesize(t1.getText(), t2.getText(), slider.getValue());
+        progressBar.setProgress(1);
         jLabel1.setText("Projected New Likes by algo1 = " + countlikesFrom1 + " from 2: " + countlikesFrom2 + " from 3: " + countlikesFrom3);
         
         // plot data
@@ -123,4 +143,33 @@ public class StartFXMLController implements Initializable {
         FBHashGet.numberLikes.clear();
     }
     
+    @FXML
+    private void UploadButtonClick(ActionEvent event) {
+        try
+        {
+            Node node = (Node) event.getSource();
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Open File");
+            File x = chooser.showOpenDialog(node.getScene().getWindow());
+            if (x == null) 
+            {
+                imagePath = "NULL";
+            }
+            else           
+            {
+                File dest = new File("./src//1.png");
+                imagePath = x.getPath();
+                Files.copy(x.toPath(), dest.toPath(), REPLACE_EXISTING);
+                Image img = new Image("1.png");
+                iv1.setImage(img);
+            }
+
+            t1.setDisable(true);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error in Displaying Photo!!!");
+        }
+        
+    }
 }
